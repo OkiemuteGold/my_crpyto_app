f57d1703d3298ccc8f49f2ed4b2c021e57ae1829
Okiemute Gold <goldokiemute@gmail.com>
2022-11-10 14:51:13 +0100

    used env

--- key.js
+++
@@ -1,3 +0,0 @@
-export default {
-    key: 'jePLt9Fvuo5oTyPFD75Qx6y0lMa1qfIy4LmujaXT'
-}
--- src/store/state.js
+++ src/store/state.js
@@ -1,11 +1,7 @@
-
-// const API_KEY = process.env.API_KEY;
-import API_KEY from "../../key";
-
 export default {
     baseURL: "https://yfapi.net",
     baseCryptoURL: "https://api.blockchain.com/v3/exchange",
-    apiKey: API_KEY.key,
+    apiKey: process.env.VUE_APP_API_KEY,
     stockDetails: [],
     companyInfo: [],
     chartDetails: [],
